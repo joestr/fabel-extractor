9a7287aa81dcfb901c23a8dc3d9eada363faad06
Joel Strasser <strasser999@gmail.com>
2021-01-20 17:59:00 +0100

    init

---
+++ fabel-extractor/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Configuration;
+using System.Collections.Specialized;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace fabel_extractor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			NameValueCollection config;
+			config = ConfigurationManager.AppSettings;
+
+			if (bool.Parse(config.Get("verbose_local_output_only"))) {
+				log("VERBOSE", "LOCAL OUTPUT ONLY");
+
+				PcTerminalData localdataManager = new PcTerminalData(
+					config.Get("pcterminal_connectionstring"),
+					config.Get("pcterminal_query")
+				);
+
+				List<IList<object>> localData = localdataManager.GetDataAsTableForSpreadsheet();
+
+				log("INFO", "---+++---+++---");
+				foreach (List<object> localrow  in localData) {
+					foreach (object column in localrow) {
+						Console.Write("'" + column + "';");
+					}
+					Console.Write("\n");
+				}
+				log("INFO", "---+++---+++---");
+				Console.ReadKey();
+				System.Environment.Exit(0);
+			}
+
+			log("INFO", "Creating SpreadsheetManager ...");
+
+			SpreadsheetManager spreadsheetManager = new SpreadsheetManager(
+				config.Get("spreadsheet_secretsfile"),
+				config.Get("spreadsheet_appname"),
+				config.Get("spreadsheet_id"),
+				config.Get("spreadsheet_sheetname")
+			);
+
+			log("INFO", "Created SpreadsheetManager.");
+
+			log("INFO", "Creating PcTerminalData manager ...");
+
+			PcTerminalData dataManager = new PcTerminalData(
+				config.Get("pcterminal_connectionstring"),
+				config.Get("pcterminal_query")
+			);
+
+			log("INFO", "Created PcTerminalData.");
+
+			while(true)
+			{
+				log("INFO", "Clearing all data from spreadsheet ...");
+
+				spreadsheetManager.ClearAll();
+
+				log("INFO", "Cleared all data from spreadsheet.");
+
+				log("INFO", "Getting info and uploading ...");
+
+				spreadsheetManager.FillAll(dataManager.GetDataAsTableForSpreadsheet());
+
+				log("INFO", $"Finished! Again in {config.Get("app_sleep_minutes")} minutes");
+
+				Thread.Sleep(TimeSpan.FromMinutes(Int32.Parse(config.Get("app_sleep_minutes"))));
+			}
+		}
+
+		static void log(string level, string message)
+		{
+			Console.WriteLine($"[{string.Concat(DateTime.UtcNow.ToString("s"), "Z")}] [{level}]: {message}");
+		}
+	}
+}
---
+++ fabel-extractor/SpreadsheetManager.cs
@@ -0,0 +1,68 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using System.Collections.Generic;
+using System.IO;
+
+namespace fabel_extractor
+{
+	class SpreadsheetManager
+	{
+		private string[] scopes = new string[]
+		{
+			SheetsService.Scope.Spreadsheets
+		};
+		private GoogleCredential googleCredential;
+		private SheetsService spreadsheetService;
+		private string appCredentialPath, appName, spreadsheetId, spreadsheetSheetName;
+
+		public SpreadsheetManager(string appCredentialPath, string appName, string spreadsheetId, string spreadsheetSheetName)
+		{
+			this.appCredentialPath = appCredentialPath;
+			this.appName = appName;
+			this.spreadsheetId = spreadsheetId;
+			this.spreadsheetSheetName = spreadsheetSheetName;
+
+			AuthAndInit();
+		}
+
+		private void AuthAndInit()
+		{
+			using (var stream = new FileStream(this.appCredentialPath, FileMode.Open, FileAccess.Read))
+			{
+				this.googleCredential = GoogleCredential.FromStream(stream)
+				.CreateScoped(scopes);
+			}
+
+			this.spreadsheetService = new SheetsService(
+				new BaseClientService.Initializer()
+				{
+					HttpClientInitializer = this.googleCredential,
+					ApplicationName = this.appName
+				}
+			);
+		}
+
+		public void ClearAll()
+		{
+			var range = $"{this.spreadsheetSheetName}!A:Z";
+			var requestBody = new ClearValuesRequest();
+
+			var deleteRequest = this.spreadsheetService.Spreadsheets.Values.Clear(requestBody, spreadsheetId, range);
+			var deleteReponse = deleteRequest.Execute();
+		}
+
+		public void FillAll(List<IList<object>> obs)
+		{
+			var range = $"{this.spreadsheetSheetName}!A:Z";
+			var valueRange = new ValueRange();
+
+			valueRange.Values = obs;
+
+			var appendRequest = this.spreadsheetService.Spreadsheets.Values.Append(valueRange, spreadsheetId, range);
+			appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+			var appendReponse = appendRequest.Execute();
+		}
+	}
+}
